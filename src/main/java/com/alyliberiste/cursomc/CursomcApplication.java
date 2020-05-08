db195eab95ad52eedcbd350ef091d1c4aa34d326
Aly Liberiste <alyliberiste38@gmail.com>
2020-05-07 21:08:57 -0300

    Operação de inserção criada

--- src/main/java/com/alyliberiste/cursomc/CursomcApplication.java
+++ src/main/java/com/alyliberiste/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.alyliberiste.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.alyliberiste.cursomc.domain.Categoria;
+import com.alyliberiste.cursomc.repositorires.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//instanciações
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		//vai inserir as cat no BD
 	}
 
 }
