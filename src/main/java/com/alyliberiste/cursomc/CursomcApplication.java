d3b2a7f0780a12739b1daa54435286e4c8735c12
Aly Liberiste <alyliberiste38@gmail.com>
2020-05-16 21:33:56 -0300

    Entidades ItemPedido e ItemPedidoPK

--- src/main/java/com/alyliberiste/cursomc/CursomcApplication.java
+++ src/main/java/com/alyliberiste/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.alyliberiste.cursomc.domain.Cidade;
 import com.alyliberiste.cursomc.domain.Cliente;
 import com.alyliberiste.cursomc.domain.Endereco;
 import com.alyliberiste.cursomc.domain.Estado;
+import com.alyliberiste.cursomc.domain.ItemPedido;
 import com.alyliberiste.cursomc.domain.Pagamento;
 import com.alyliberiste.cursomc.domain.PagamentoComBoleto;
 import com.alyliberiste.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.alyliberiste.cursomc.repositories.CidadeRepository;
 import com.alyliberiste.cursomc.repositories.ClienteRepository;
 import com.alyliberiste.cursomc.repositories.EnderecoRepository;
 import com.alyliberiste.cursomc.repositories.EstadoRepository;
+import com.alyliberiste.cursomc.repositories.ItemPedidoRepository;
 import com.alyliberiste.cursomc.repositories.PagamentoRepository;
 import com.alyliberiste.cursomc.repositories.PedidoRepository;
 import com.alyliberiste.cursomc.repositories.ProdutoRepository;
@@ -32,6 +34,7 @@ import com.alyliberiste.cursomc.repositories.ProdutoRepository;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
+	//dependencias
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
@@ -48,6 +51,8 @@ public class CursomcApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -119,9 +124,21 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
-
-
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1,0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));	
+		
+
 	}
 
 }
