e0ef604020078bbde354262057f29d83e7608364
Aly Liberiste <alyliberiste38@gmail.com>
2020-05-22 17:57:24 -0300

    Subtotal e Total do Pedido

--- src/main/java/com/alyliberiste/cursomc/domain/Pedido.java
+++ src/main/java/com/alyliberiste/cursomc/domain/Pedido.java
@@ -56,6 +56,14 @@ public class Pedido implements Serializable{
 		//tirei pgmto como obrigatorio para deixar pedido e pagamto ind
 	}
 
+	public double getValorTotal() {
+		double soma = 0.0;
+		for(ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
