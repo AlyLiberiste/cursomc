1e982ff097235ac6258044c3b1d0c9595eb6f680
Aly Liberiste <alyliberiste38@gmail.com>
2020-07-03 12:54:44 -0300

    Profile de desenvolvimento criado

--- src/main/java/com/alyliberiste/cursomc/config/DevConfig.java
+++ src/main/java/com/alyliberiste/cursomc/config/DevConfig.java
@@ -1,8 +1,9 @@
-/*package com.alyliberiste.cursomc.config;
+package com.alyliberiste.cursomc.config;
 
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,11 +17,18 @@ public class DevConfig {
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
 			
 		dbService.instantiateTestDatabase();
 		return true;
 	}
 
-}*/
+}
